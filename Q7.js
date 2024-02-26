54a55154239636cb3cbd762e82caffb9bda1a8d4
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-26 23:27:36 +0500

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,11 @@
+// 7. Name Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the Results .
+var a = 10;
+var b = 5;
+""; // Addition :-
+console.log(a + b);
+// Subtraction :-
+console.log(a - b);
+// Multiplication :-
+console.log(a * b);
+// Divide :-
+console.log(a / b);
---
+++ Q7.ts
@@ -0,0 +1,17 @@
+// 7. Name Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the Results .
+
+
+let a: number = 10;
+let b: number = 5;
+
+``// Addition :-
+console.log(a + b)
+
+// Subtraction :-
+console.log(a - b)
+
+// Multiplication :-
+console.log(a * b)
+
+// Divide :-
+console.log(a / b)
